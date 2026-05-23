b713bf379372c7cff4f46934b01172b311f08e6e
Aryavart Chandel <aryavartchandel2@gmail.com>
2026-05-23 23:55:51 +0530

    Fixed TMDB routes

--- backend/routes/movieRoutes.js
+++ backend/routes/movieRoutes.js
@@ -1,15 +1,28 @@
 const express = require('express')
+
 const {
   createMovie,
   deleteMovie,
   getMovieById,
   getMovies,
   updateMovie,
+  searchMovies,
+  getMovieDetails,
 } = require('../controllers/movieController')
 
 const router = express.Router()
 
 router.route('/').get(getMovies).post(createMovie)
-router.route('/:id').get(getMovieById).patch(updateMovie).delete(deleteMovie)
+
+// TMDB
+router.get('/search', searchMovies)
+router.get('/tmdb/:id', getMovieDetails)
+
+// CRUD
+router
+  .route('/:id')
+  .get(getMovieById)
+  .patch(updateMovie)
+  .delete(deleteMovie)
 
 module.exports = router
